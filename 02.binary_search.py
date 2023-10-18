334d0da03f935696b1263f6da23158fd175c48b3
Nikolas Nakov <Nikolas.Nakov@indeavr.com>
2023-10-18 10:58:42 +0300

    Added comment

---
+++ 02.binary_search.py
@@ -0,0 +1,15 @@
+def binary_search(list, start, end, search):
+    #find the mid point
+    if end >= start:
+        mid = start + (end - start) // 2
+        
+    if list[mid] == search:
+        return f'The searched item: {str(search)} is at position: {str(mid + 1)}'
+    elif list[mid] > search:
+        return binary_search(list, start, mid-1, search)
+    elif list[mid] < search:
+        return binary_search(list, mid+1, end, search)
+    else:
+        return f'The searched item: {str(search)} is not in the list'
+    
+    
